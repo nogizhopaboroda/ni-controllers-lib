79ac7af6809979f3bfa2c35e4b2b023013cacc51
Andrew Sutherland <asutherland@asutherland.org>
2018-05-06 17:23:21 -0400

    in-progress refactoring to support mk3
    F1's tests/f1_app displays output and doesn't crash on input.  Close
    to fully operational again but I want to do some more style cleanup
    and want the renames to get logged; led-rgb already drifted.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Traktor F1 and Maschine MK3 controller access
+
+This is a forked version of https://github.com/met5678/node-traktor-f1 with
+the following changes:
+- Minor bit-rot fixes and cleanup of things that appeared to be in-progress
+  refactoring.
+- Maschine MK3 support (currently in-progress)
+- Some minimal attempts at making things slightly more generic.  There was
+  already a forward-looking "traktor_f1_config.json", I'm just expanding on
+  that, possibly in a silly way.
--- tests/mk3raw.js
+++ tests/mk3raw.js
@@ -9,20 +9,36 @@ device.on("error",  (ex) => {
 });
 
 const pkt = new Array(42);
-// Pads, sliders
 pkt[0] = 0x81;
+// Touch slider mini-dot LEDs which don't seem to support the "white" values.
 for (let i = 1; i < 26; i++) {
   pkt[i] = i + 3;
+  //pkt[i] = i + 3 + 26;
 }
+// Drum pads.
 for (let i = 26; i < 42; i++) {
-  pkt[i] = (i-25) * 4 + 2;
+  pkt[i] = (i-26 + 1) * 4 + 2;
+  //pkt[i] = (i-25) + 3 + 32;
 }
 
 device.write(pkt);
 
 const pkt2 = new Array(63);
 pkt2[0] = 0x80;
+// All buttons
 for (let i = 1; i < pkt2.length; i++) {
   pkt2[i] = 63;
 }
+// Group pads.
+for (let i = 30; i <= 37; i++) {
+  // whites
+  pkt2[i] = i%4 + 68;
+  // show contiguous colors with an offset.
+  pkt2[i] = i-30 + 4 + 12;
+}
+// Directional encoder dots
+for (let i = 59; i <= 62; i++) {
+  pkt2[i] = i%4 + 4;
+}
+pkt2[6] = 68;
 device.write(pkt2);
