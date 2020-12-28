49a7c18821c18cfc9a79f78f7823435cb01b2540
Alexander Zhukov <alexander.zhukov@native-instruments.de>
2020-12-28 01:14:33 +0100

    fix web demo

--- examples/maschine_mk2_mikro/web.ts
+++ examples/maschine_mk2_mikro/web.ts
@@ -1,19 +1,13 @@
-import { MaschineMk3 } from "../../lib/maschine_mk3";
+import { MaschineMk2Mikro } from "../../lib/maschine_mk2_mikro";
 import { createWebHidAdapter, createWebUsbAdapter } from "../../lib/usb/web";
-import urlPlaceholderJpeg from "./placeholder.jpeg";
 import { runDemo } from "./runDemo";
 
-fetch(urlPlaceholderJpeg)
-  .then((response) => response.arrayBuffer())
-  .then((jpegData) => {
-    document.addEventListener("click", () => {
-      document.body.innerHTML = "Running";
-      runDemo(
-        new MaschineMk3(createWebHidAdapter, createWebUsbAdapter),
-        jpegData
-      ).catch((error) => {
-        document.body.innerHTML = `Error: ${error.message}`;
-        console.error(error);
-      });
-    });
-  });
+document.addEventListener("click", () => {
+  document.body.innerHTML = "Running";
+  runDemo(new MaschineMk2Mikro(createWebHidAdapter, createWebUsbAdapter)).catch(
+    (error) => {
+      document.body.innerHTML = `Error: ${error.message}`;
+      console.error(error);
+    }
+  );
+});
