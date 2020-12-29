50798280e023936b3cf850cb8f77eae7e2bea3c0
Alexander Zhukov <alexander.zhukov@native-instruments.de>
2020-12-29 22:42:41 +0100

    extend rgb_led class

--- lib/components/output/led_rgb.ts
+++ lib/components/output/led_rgb.ts
@@ -1,5 +1,24 @@
 import tinycolor from "tinycolor2";
 
+const colorTable = [
+  "red",
+  "orangered",
+  "darkorange",
+  "yellow", // in hue space this would just be yellow
+  "lime", // in hue space this would be lime
+  "green", // in hue space this would be green
+  "springgreen", // in hue space this would be mint
+  "aquamarine", // in hue space this would be a greenish-cyan
+  "cyan",
+  "turquoise",
+  "blue",
+  "plum",
+  "violet",
+  "purple",
+  "magenta",
+  "fuchsia",
+];
+
 export interface LedRgbConfig {
   bAddr: number;
   gAddr: number;
@@ -37,5 +56,22 @@ export class LED_RGB {
     this.g = color.g;
     this.b = color.b;
     this.updateOutputPacket();
+  }
+
+
+  setWhiteBrightness(f: number) {
+    const color = tinycolor({ r: this.r, g: this.g, b: this.b });
+    const { r, g, b } = color.brighten(f).toRgb();
+    this.setRGB(r, g, b);
+  }
+
+  setColorByNumberHelper(color16: number){
+    this.setColorHelper(colorTable[color16]);
+  }
+
+  setColorHelper(colorString: string){
+    const color = tinycolor(colorString);
+    const { r, g, b } = color.toRgb();
+    this.setRGB(r, g, b);
   }
 }
